ef0bd2e42cf5242c1e953b097b108ba39b20ede8
Z Zhang <zuoning@AndrewZs-MacBook-Air.local>
2023-04-22 13:40:52 -0700

    fail running map

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,9 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // GMSServices.provideAPIKey("AIzaSyDDIBQQfDacQ3D8P2fivTGqpa4ESfwQ1p4")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+// AIzaSyDDIBQQfDacQ3D8P2fivTGqpa4ESfwQ1p4
