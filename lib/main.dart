3a9da5ac5fe91d60ac9dd05a70af9fdff97bb2c3
Satyen Subramaniam <satyensubramaniam@gmail.com>
2023-04-22 14:35:13 -0700

    Adding Authentication (Email)

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:safe_alert/firebase_options.dart';
-import 'package:safe_alert/screens/ContactView.dart';
-import 'package:safe_alert/screens/home_screen.dart';
-import 'screens/TextPage.dart';
+import 'screens/LogIn.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,18 +32,10 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: ContactView(update: _updateContact),
+      home: Scaffold(body: LoginScreen()),
+      //home: ContactView(update: _updateContact),
     );
   }
 }
