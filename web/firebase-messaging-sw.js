628c5532eacaed5cc8fdfe4b007d1ac811bf351f
tranbrandon1233 <70561420+tranbrandon1233@users.noreply.github.com>
2024-01-24 14:40:03 -0800

    Update firebase-messaging-sw.js

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -2,14 +2,14 @@ importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
 
 firebase.initializeApp({
-    apiKey: 'AIzaSyD2Tw95W_WH7HbHtB5EQweaBAvoeqAvtZE',
-    appId: '1:826371401282:web:8d52d6005653fb408eafdb',
-    messagingSenderId: '826371401282',
-    projectId: 'lahack-14f69',
-    authDomain: 'lahack-14f69.firebaseapp.com',
-    databaseURL: 'https://lahack-14f69-default-rtdb.firebaseio.com',
-    storageBucket: 'lahack-14f69.appspot.com',
-    measurementId: 'G-QVCLG7NB3K',
+    apiKey: process.env.apiKey,
+    appId: process.env.appId,
+    messagingSenderId: process.env.messagingSenderId,
+    projectId: process.env.projectId,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    storageBucket: process.env.storageBucket,
+    measurementId: process.env.measurementId,
 });
 
 const messaging = firebase.messaging();
